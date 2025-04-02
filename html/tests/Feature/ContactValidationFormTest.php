7abae2eeb01a73745229426e5eaab3d3c057deda
Igor Simões <igorsimoes_96@hotmail.com>
2025-04-02 18:39:11 +0100

    add validations tests for contacts

---
+++ html/tests/Feature/ContactValidationFormTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContactValidationFormTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_name_is_required(): void
+    {
+        $response = $this->post('/contacts', [
+            'email' => 'diane@example.com',
+            'contact' => '111111111',
+        ]);
+
+        $response->assertSessionHasErrors('name');
+    }
+
+    public function test_name_size(): void
+    {
+        $response = $this->post('/contacts', [
+            'name' => 'diane',
+            'email' => 'diane@example.com',
+            'contact' => '111111111',
+        ]);
+
+        $response->assertSessionHasErrors('name');
+    }
+
+    public function test_email_is_required()
+    {
+        $response = $this->post('/contacts', [
+            'name' => 'Diane Test',
+            'contact' => '111111111',
+        ]);
+
+        $response->assertSessionHasErrors('email');
+    }
+
+    public function test_email_is_valid()
+    {
+        $response = $this->post('/contacts', [
+            'name' => 'Diane Test',
+            'email' => 'diane.email',
+            'contact' => '111111111',
+        ]);
+
+        $response->assertSessionHasErrors('email');
+    }
+
+    public function test_contact_is_required()
+    {
+        $response = $this->post('/contacts', [
+            'name' => 'Diane Test',
+            'email' => 'diane@example.com',
+        ]);
+
+        $response->assertSessionHasErrors('contact');
+    }
+}
